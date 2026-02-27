ce537a710e80811a86bff929e970ca423ad52c29
Austin <austin@catalyst.com.co>
2026-02-27 09:33:22 -0700

    Tighten canvas height bounding logic so that total usage perfectly tracks the edge of placed logos

--- src/app/gangsheet/[shopId]/page.tsx
+++ src/app/gangsheet/[shopId]/page.tsx
@@ -39,17 +39,7 @@ export default function GangsheetBuilderApp() {
 
         const maxBottomInches = Math.ceil(maxBottom / CANVAS_PIXELS_PER_INCH);
 
-        setCanvasHeightInches(prevHeight => {
-            // Expand down if they get close to the edge
-            if (maxBottomInches > prevHeight - 5) {
-                return Math.max(24, maxBottomInches + 12);
-            }
-            // Shrink back up if they delete things (don't shrink smaller than 24)
-            if (maxBottomInches < prevHeight - 17 && prevHeight > 24) {
-                return Math.max(24, maxBottomInches + 12);
-            }
-            return prevHeight;
-        });
+        setCanvasHeightInches(Math.max(24, maxBottomInches));
     }, [artworks]);
 
     const repackCanvas = (currentArts: ArtworkInstance[], gapInches: number) => {
